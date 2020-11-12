8c66a60b8a8ff1204e4e49130fa4a657a915df7f
sanjuthomas920 <sanjuthomas920@gmail.com>
2020-11-12 20:51:59 +0000

    Add all the required code for the game

---
+++ MineSweeperGame/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace MineSweeperGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Windows Form Designer generated code
+    
+
+
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblLife = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(17, 16);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(327, 303);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(371, 16);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(131, 48);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start Game";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(373, 113);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Score";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblScore.Location = new System.Drawing.Point(475, 113);
+            this.lblScore.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(26, 29);
+            this.lblScore.TabIndex = 2;
+            this.lblScore.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(373, 155);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 26);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Life";
+            // 
+            // lblLife
+            // 
+            this.lblLife.AutoSize = true;
+            this.lblLife.BackColor = System.Drawing.Color.YellowGreen;
+            this.lblLife.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblLife.Location = new System.Drawing.Point(475, 155);
+            this.lblLife.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblLife.Name = "lblLife";
+            this.lblLife.Size = new System.Drawing.Size(26, 29);
+            this.lblLife.TabIndex = 4;
+            this.lblLife.Text = "3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 334);
+            this.Controls.Add(this.lblLife);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Minefield";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+#endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblLife;
+    }
+
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    
+        #endregion
+    
+}
---
+++ MineSweeperGame/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MineSweeperGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Aim is to click 10 green boxes and you have 3 lifes", "Game Rule", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (result == DialogResult.Yes)
+            {
+                Random rnd = new Random();
+                int mine1 = rnd.Next(1, 12);
+                int mine2 = rnd.Next(2, 4);
+                int mine3 = rnd.Next(9, 13);
+                int mine4 = rnd.Next(13, 18);
+                int mine5 = rnd.Next(19, 25);
+                int mine6 = rnd.Next(12, 20);
+                int mine7 = rnd.Next(6, 15);
+
+                for (int i = 1; i <= 25; i++)
+                {
+                    Button btnTemp = new Button();
+
+                    btnTemp.Name = "btnTemp" + i.ToString();
+                    btnTemp.Size = new System.Drawing.Size(40, 40);
+                    btnTemp.Text = "$$";
+                    btnTemp.UseVisualStyleBackColor = true;
+
+                    if (mine1 == i || mine2 == i || mine3 == i || mine4 == i || mine5 == i || mine6 == i || mine7 == i)
+                        btnTemp.Tag = true;
+                    else
+                        btnTemp.Tag = false;
+
+                    btnTemp.Click += BtnTemp_Click;
+                    flowLayoutPanel1.Controls.Add(btnTemp);
+                }
+            }
+            else
+                Close();
+        }
+
+
+
+        private void BtnTemp_Click(object sender, EventArgs e)
+        {
+            Button btnTemp = (Button)sender;
+            bool tag = (bool)btnTemp.Tag;
+            int life = int.Parse(lblLife.Text);
+            int score = int.Parse(lblScore.Text);
+
+            if (tag)
+            {
+                btnTemp.BackColor = Color.Red;
+                life--;
+                LostGame(life, score);
+
+            }
+            else
+            {
+                GoToNextMove(score, life);
+                btnTemp.BackColor = Color.Green;
+
+            }
+
+        }
+
+        public void LostGame(int life, int score)
+        {
+
+            if (life == 0)
+            {
+                DialogResult result = MessageBox.Show("Boom !!! You are lost\nRestart ?", "Game Result", MessageBoxButtons.YesNo, MessageBoxIcon.Stop);
+                if (result == DialogResult.Yes)
+                    Application.Restart();
+                else
+                    Close();
+            }
+            else
+            {
+                score--;
+                GoToNextMove(score, life);
+            }
+        }
+
+        public void GoToNextMove(int score, int life)
+        {
+            score++;
+            if (score >= 10)
+            {
+                DialogResult result = MessageBox.Show("Congrats!! You won the Game", "Game Result", MessageBoxButtons.YesNo, MessageBoxIcon.None);
+                if (result == DialogResult.Yes)
+                    Application.Restart();
+                else
+                    Close();
+            }
+            lblScore.Text = score.ToString();
+            lblLife.Text = life.ToString();
+
+        }
+    }
+}
