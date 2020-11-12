0965c10fd3f7f6be43c8fcb67dfcf5a85728cfcf
sanjuthomas920 <sanjuthomas920@gmail.com>
2020-11-12 20:58:13 +0000

    Add all the required code for the game

---
+++ MineSweeperGameTest/MineSweeperTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MineSweeperGame;
+
+
+namespace MineSweeperGameTest
+{
+    [TestClass]
+    public class MineSweeperTest
+    {
+        [TestMethod]
+        public void LostGame_withNoLife()
+        {
+            //Arrange
+            Form1 fm = new Form1();
+
+            //Act
+            fm.LostGame(0, 7);
+
+            //Assert
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void LostGame_WithLife()
+        {
+            //Arrange
+            Form1 fm = new Form1();
+
+            //Act
+            fm.LostGame(1, 7);
+
+            //Assert
+            fm.GoToNextMove(7, 1);
+        }
+        [TestMethod]
+        public void GoToNextMove_ScoreGreaterThan10_Winner()
+        {
+            //Arrange
+            Form1 fm = new Form1();
+            //Act
+            fm.GoToNextMove(11, 1);
+            //Assert
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void GoToNextMove_ScoreLessThan10()
+        {
+            //Arrange
+            Form1 fm = new Form1();
+            //Act
+            fm.GoToNextMove(7, 1);
+            //Assert
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ MineSweeperGameTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MineSweeperGameTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MineSweeperGameTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8c330cb4-9955-4e46-a6b2-e48beeffe111")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
